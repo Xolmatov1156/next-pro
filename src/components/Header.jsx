3c962048fbeab25f4d74001b342b8ac39460f705
Ulugbek Xolmatov <ulugbekxolmatov@MacBook-Air-Ulugbek.local>
2024-10-07 12:50:52 +0500

    next

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,32 +1,39 @@
-import Link from 'next/link'
-import React from 'react'
-import Image from 'next/image'
-import Logo from '../assets/logo.svg'
-import AddUser from '../assets/add-user.svg'
-import Search from '../assets/search.svg'
-import Like from '../assets/like.svg'
-import Shop from '../assets/shop.svg'
+"use client";
+
+import Link from 'next/link';
+import React from 'react';
+import Image from 'next/image';
+import Logo from '../assets/logo.svg';
+import AddUser from '../assets/add-user.svg';
+import Search from '../assets/search.svg';
+import Like from '../assets/like.svg';
+import Shop from '../assets/shop.svg';
+import { usePathname } from 'next/navigation';
 
 function Header() {
+  const pathname = usePathname();
+  
+  const isActive = (path) => {
+    return path === pathname ? "text-gray-500 underline" : "";
+  };
+
   return (
-    <>
     <header className='w-[1286px] ml-[54px] flex py-[30px] items-center'>
-    <Image src={Logo} alt="logo" />
-    <div className="ml-[266px] w-[430px] flex justify-between">
-    <Link href={'/'}>Home</Link>
-    <Link href={'/shop'}>Shop</Link>
-    <Link href={'/contact'}>Contact</Link>
-    <Link href={'/blog'}>Blog</Link>
-    </div>
-    <div className={`ml-[158px] flex space-x-[45px]`}>
-    <Image src={AddUser} alt="add user" className='cursor-pointer' />
-    <Image src={Search} alt="search" className='cursor-pointer' />
-    <Image src={Like} alt="like" className='cursor-pointer' />
-    <Image src={Shop} alt="shop" className='cursor-pointer' />
-    </div>
+      <Image src={Logo} alt="logo" />
+      <div className="ml-[266px] w-[430px] flex justify-between">
+        <Link href='/' className={`hover:text-gray-500 hover:underline ${isActive('/')}`}>Home</Link>
+        <Link href='/shop' className={`hover:text-gray-500 hover:underline ${isActive('/shop')}`}>Shop</Link>
+        <Link href='/contact' className={`hover:text-gray-500 hover:underline ${isActive('/contact')}`}>Contact</Link>
+        <Link href='/blog' className={`hover:text-gray-500 hover:underline ${isActive('/blog')}`}>Blog</Link>
+      </div>
+      <div className="ml-[158px] flex space-x-[45px]">
+        <Image src={AddUser} alt="add user" className='cursor-pointer' />
+        <Image src={Search} alt="search" className='cursor-pointer' />
+        <Image src={Like} alt="like" className='cursor-pointer' />
+        <Image src={Shop} alt="shop" className='cursor-pointer' />
+      </div>
     </header>
-    </>
-  )
+  );
 }
 
-export default Header
+export default Header;
